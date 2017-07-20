909069792132d73cb714756853feb980cab282ce
TheMonkeyD <cherfaoui.ghiles@gmail.com>
2017-07-20 22:51:49 +0200

    conflit merge

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-	header('location:./views/index.php');
-
-		
--- src/includes/fonctions.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function debug($array){
-		echo("<div class='well'><pre>".var_dump($array)."</pre></div>");
-	}
-	function diebug($array){
-		echo("<div class='well'><pre>".var_dump($array)."</pre></div>");
-		die();
-	}
